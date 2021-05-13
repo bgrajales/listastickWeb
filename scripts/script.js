39add19867181c3b6b53a7b7e165018f23d732b9
bgrajales <82546939+bgrajales@users.noreply.github.com>
2021-05-13 12:22:21 -0300

    JS code for basic funcionality of login and register pages

--- scripts/script.js
+++ scripts/script.js
@@ -1,53 +1,164 @@
 //Funcionamiento interno del Login Form
 
-const loginForm = document.getElementById("loginForm")
 const loginButton = document.getElementById("login-form-submit");
 const loginErrorMsg = document.getElementById("login-error-msg");
 
-loginButton.addEventListener("click", (action) => {
-    action.preventDefault();
+if (loginButton != null) {
+    
+    loginButton.addEventListener("click", (action) => {
+        action.preventDefault();
 
-    const mail = document.getElementById("mail").value
-    const password = document.getElementById("password").value
+        const mail = document.getElementById("mail").value
+        const password = document.getElementById("password").value
 
-    console.log(mail, password)
+        console.log(mail, password)
+        console.log(loginErrorMsg.innerHTML)
+        if (mail === "listastickDev" && password === "webdev") {
+            window.location.href = "home.html"
+        } else if (mail === "listastickDev") {
+            loginErrorMsg.style.display = "block";
+            loginErrorMsg.innerHTML = "Error: Wrong password"
+        } else {
+            loginErrorMsg.style.display = "block";
+            loginErrorMsg.innerHTML = "Error: User does not exist" 
+        }
+   
+    })
 
-    if (mail === "bgrajales97@gmail.com" && password === "webdev") {
-        window.location.href = "home.html"
-    } else {
-        loginErrorMsg.style.display = "block"; 
+    var input1 = document.getElementById("password");
+
+    input1.addEventListener("keyup", function(event) {
+
+        if (event.keyCode === 13) {
+
+            event.preventDefault();
+
+            document.getElementById("login-form-submit").click();
     }
-})
+    })
 
-var input1 = document.getElementById("password");
+    var input2 = document.getElementById("login-form-submit");
 
-input1.addEventListener("keyup", function(event) {
+    input1.addEventListener("keyup", function(event) {
 
-    if (event.keyCode === 13) {
+        if (event.keyCode === 13) {
 
-        event.preventDefault();
+            event.preventDefault();
 
-        document.getElementById("login-form-submit").click();
-  }
-})
+            document.getElementById("login-form-submit").click();
+        }
+    })
 
-var input2 = document.getElementById("login-form-submit");
-
-input1.addEventListener("keyup", function(event) {
-
-    if (event.keyCode === 13) {
-
-        event.preventDefault();
-
-        document.getElementById("login-form-submit").click();
-  }
-})
+}
 
 // Termina funcionamiento interno de LoginForm
 
 
 // Funcionamiento interno de RegisterForm
 
-const registerForm = document.getElementById("registerForm")
-const loginButton = document.getElementById("register-form-submit");
-const loginErrorMsg = document.getElementById("register-error-msg");
+const registerButton = document.getElementById("register-form-submit");
+const registerErrorMsg = document.getElementById("register-error-msg");
+
+if (registerButton != null) {
+
+    registerButton.addEventListener("click", (action) => {
+        action.preventDefault();
+
+        const email = document.getElementById("email").value
+        const fullName = document.getElementById("fullName").value
+        const password = document.getElementById("password").value
+        const repeatPassword = document.getElementById("repeatPassword").value
+        let errorCount = 0
+
+        if (!checkEmailValidity(email)) {
+            registerErrorMsg.style.display = "block"
+            document.getElementById("email").style = "box-shadow: rgb(181 49 49) 0px 0px 0px 5px;"
+            errorCount++;
+        } else {
+            document.getElementById("email").style = "box-shadow: none"
+        }
+
+        if (!checkFullName(fullName)) {
+            registerErrorMsg.style.display = "block"
+            document.getElementById("fullName").style = "box-shadow: rgb(181 49 49) 0px 0px 0px 5px;"
+            errorCount++;
+        } else {
+            document.getElementById("fullName").style = "box-shadow: none"
+        }
+
+        if (!checkPassword(password)) {
+            registerErrorMsg.style.display = "block"
+            document.getElementById("password").style = "box-shadow: rgb(181 49 49) 0px 0px 0px 5px;"
+            errorCount++;
+        } else {
+            document.getElementById("password").style = "box-shadow: none"
+        }
+
+        if (password != repeatPassword) {
+            registerErrorMsg.style.display = "block"
+            document.getElementById("repeatPassword").style = "box-shadow: rgb(181 49 49) 0px 0px 0px 5px;"
+            errorCount++;
+        } else {
+            document.getElementById("repeatPassword").style = "box-shadow: none"
+        }
+        
+        if (errorCount === 0) {
+            window.location.href = "home.html"
+        } else {
+            loginErrorMsg.style.display = "block"; 
+        }
+})
+
+    var input1 = document.getElementById("repeatPassword");
+
+    input1.addEventListener("keyup", function(event) {
+
+        if (event.keyCode === 13) {
+
+            event.preventDefault();
+
+            document.getElementById("register-form-submit").click();
+    }
+    })
+
+    var input2 = document.getElementById("register-form-submit");
+
+    input1.addEventListener("keyup", function(event) {
+
+        if (event.keyCode === 13) {
+
+            event.preventDefault();
+
+            document.getElementById("register-form-submit").click();
+    }
+    })
+
+}
+
+// Checking if the email of Register Input is correct
+function checkEmailValidity(email) {
+ if (/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(email)) {
+    return (true)
+  } else {
+    return (false)
+  }
+}
+
+// Checking if the name of Register Input is correct
+function checkFullName(fullName) {
+    if (/^[a-zA-Z]+( [a-zA-Z]+)+$/.test(fullName)) {
+        return true
+    } else {
+        return false
+    }
+}
+
+// Checking if password is longer than 8 characters
+function checkPassword(password) {
+    if(password.length < 8) {   
+        return false;  
+     }  else {
+        return true;
+     }
+}
+
